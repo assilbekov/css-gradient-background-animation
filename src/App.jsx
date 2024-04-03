2267583dba14b5fe5428b711a71d52c32243c381
assilbekov <askhat.assilbekov@gmail.com>
2024-04-03 13:44:29 -0300

    implement html

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useRef, useState } from "react"
+
+const Feeling = {
+  Sad: 1,
+  Neutral: 2,
+  Happy: 3
+}
+
+const feelingColorMap = {
+  [Feeling.Sad]: ["#394e7a", "#8e9ac7", "#4ee"],
+  [Feeling.Neutral]: ["#22d", "#c8f8ff", "#6d2"],
+  [Feeling.Happy]: ["#39f", "#f4e54d", "#fa3"],
+}
+
+const feelingLabelMap = {
+  [Feeling.Sad]: "Could be better",
+  [Feeling.Neutral]: "Okay",
+  [Feeling.Happy]: "Great"
+}
 
 function App() {
+  const wrapperRef = useRef(null);
+  const [feeling, setFeeling] = useState(Feeling.Neutral)
+
+
+
+  useEffect(() => {
+    if (!wrapperRef.current) return
+
+    const [a, b, c] = feelingColorMap[feeling]
+
+    wrapperRef.current.style.setProperty("--color-a", a)
+    wrapperRef.current.style.setProperty("--color-b", b)
+    wrapperRef.current.style.setProperty("--color-c", c)
+  }, [feeling])
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex min-h-[100dvh] w-full items-center justify-center">
+      <div
+        ref={wrapperRef}
+        className="relative mx-auto aspect-[9/16] w-[360px] max-w-full overflow-hidden rounded-2xl bg-slate-500 text-white"
+      >
+        <div className="relative z-10">
+          <h1 className="mb-4 text-center text-2xl font-medium">How are you feeling today?</h1>
+        </div>
+
+        <h2 className="mb-4 text-center text-2xl font-medium">
+          {feelingLabelMap[feeling]}
+        </h2>
+
+        <input
+          className="range"
+          onChange={e => setFeeling(e.target.value)}
+          type="range"
+          value={feeling}
+          min={1}
+          max={3}
+          step={1}
+        />
+      </div>
+    </main>
   )
 }
 
