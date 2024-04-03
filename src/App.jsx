bdfa6cdf0003d8e7c28a330f9b31c4de603a9bce
assilbekov <askhat.assilbekov@gmail.com>
2024-04-03 14:14:40 -0300

    add after animation

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,16 @@ function App() {
     wrapperRef.current.style.setProperty("--color-c", c)
   }, [feeling])
 
+  const beforeStyle = "before:absolute before:left-[20%] before:top-[10%] before:h-[50%] before:w-[70%] before:origin-[60%] before:bg-gradient-to-br before:from-[--color-a] before:to-[--color-b] before:rounded-3xl before:blur-[50px] before:brightness-125 before:animate-blob"
+  const afterStyle = "after:absolute after:left-[50%] after:top-[30%] after:h-[70%] after:w-[70%] after:origin-[60%] after:bg-gradient-to-br after:from-[--color-a] after:to-[--color-b] after:rounded-3xl after:blur-[50px] after:brightness-125 after:animate-blob-reverse"
+
   return (
     <main className="flex min-h-[100dvh] w-full items-center justify-center">
       <div
         ref={wrapperRef}
-        className="relative mx-auto aspect-[9/16] w-[360px] max-w-full overflow-hidden rounded-2xl bg-gradient-to-br from-[--color-a] to-[--color-c] via-[--color-b] [transition-property:_--color-a,_--color-b,_--color-c] ease-in duration-500 p-8 text-white before:absolute before:left-[20%] before:top-[10%] before:h-[50%] before:w-[70%] before:origin-[60%] before:bg-gradient-to-br before:from-[--color-a] before:to-[--color-b] before:rounded-3xl before:blur-[50px] before:brightness-125 before:animate-blob"
+        className={
+          "relative mx-auto aspect-[9/16] w-[360px] max-w-full overflow-hidden rounded-2xl bg-gradient-to-br from-[--color-a] to-[--color-c] via-[--color-b] [transition-property:_--color-a,_--color-b,_--color-c] ease-in duration-500 p-8 text-white " + beforeStyle + " " + afterStyle
+        }
       >
         <div className="relative z-10">
           <h1 className="mb-12 text-5xl font-medium leading-tight">How are you feeling today?</h1>
